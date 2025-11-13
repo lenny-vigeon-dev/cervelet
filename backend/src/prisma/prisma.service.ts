24b598b18241a9b508c7ff183c9f8a1246c846e9
Ethan Nguyen <dev.ethan.nguyen@gmail.com>
2025-11-13 16:18:53 +0100

    feat(db): added prisma module & service

---
+++ backend/src/prisma/prisma.service.ts
@@ -0,0 +1,32 @@
+import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  async onModuleInit() {
+    await this.$connect();
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
+  }
+
+  async cleanDatabase() {
+    if (process.env.NODE_ENV === 'production') return;
+
+    const models = Reflect.ownKeys(this).filter((key) => key[0] !== '_');
+
+    return Promise.all(
+      models.map((modelKey) => {
+        const model = this[modelKey as keyof typeof this];
+        if (model && typeof model === 'object' && 'deleteMany' in model) {
+          // eslint-disable-next-line @typescript-eslint/no-unsafe-return, @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
+          return (model as any).deleteMany();
+        }
+      }),
+    );
+  }
+}
